db5012fafedf2f9324763d7cf3b6d89aba450bad
DkzAnderson <syndrax20@gmail.com>
2024-12-08 09:10:36 -0500

    moddified: Componente de ProductCard terminado, para móviles

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,32 +1,119 @@
+import { FaCartPlus } from "react-icons/fa6";
 
 
-interface Props {
-  name: string,
-  id: string,
-  images: string[],
-  info: {
-    name: string;
-    value: string;
-  }[];
-  poster: string;
-  price: number;
-  quanty: number;
-  requeriments: {
-    minimum: {
-      name: string;
-      value: string;
-    }[],
-    recommended: {
-      name: string;
-      value: string;
-    }[]
-  }
-  sypnosis: string[],
-  trailerUrl: string
+
+type Info = {
+  name: string;
+  value: string[] | string;
 }
 
-export const ProductCard : React.FC<Props> = ({}) => {
+type Requeriments = {
+  minimum: Info[],
+  recommended: Info[]
+}
+
+interface Props {
+  Game : {
+    name   : string
+    id     : string | number
+    images : string[]
+    info   : Info[];
+    poster : string;
+    price  : number;
+    quanty : number;
+    requeriments: Requeriments
+    sypnosis: string[];
+    gender: string[];
+    trailerUrl: string;
+  }
+}
+
+
+export const ProductCard : React.FC<Props> = ({Game}) => {
+
+  const styles = {
+    main: 'size-full relative font-roboto',
+    content: 'overflow-hidden flex flex-col rounded-lg',
+
+    posterBox: 'h-auto w-full cursor-pointer',
+    poster: 'size-full object-cover',
+
+    data: {
+      main: 'flex relative flex-col gap-0.5 px-2 py-3 dark:text-white bg-sky-200 dark:bg-black/85',
+      title: 'font-bold text-xl max-w-full truncate',
+      price: 'absolute right-2 bottom-2 text-green-800 dark:text-lime-500 font-bold text-2xl text-end',
+      weight: 'text-sm',
+      genderList: 'flex w-full gap-1.5 items-center ',
+      genderTxt: 'text-sm max-w-16 px-1 rounded bg-sky-700 dark:bg-cyan-500 font-light text-zinc-300 dark:text-zinc-900 truncate',
+      genderTitle: 'text-sm mr-1 font-bold'
+    },
+
+    cartBtn: {
+      main: 'absolute flex justify-center items-center size-14 bg-green-800 dark:bg-lime-500 rounded-full right-2 -top-7',
+      icon: 'text-3xl text-white dark:text-black mr-1 mt-1'
+    }
+
+  }
+
+
+  function GetWeight (){
+    let value: string[] | string = '' ;
+    Game.info.forEach(element => {
+      if(element.name === 'Peso'){
+        value = element.value;
+      }
+    });
+
+    return value;
+  }
+
   return (
-    <div>ProductCard</div>
+    <article className={styles.main}>
+      <div className={styles.content}>
+        {/* Poster */}
+        <picture className={styles.posterBox}>
+          <img
+            className={styles.poster} 
+            src={Game.poster} 
+            alt={Game.name+'-poster'} 
+          />
+        </picture>
+        {/* Datos */}
+        <div className={styles.data.main}>
+          <button
+            className={styles.cartBtn.main}
+          >
+            <FaCartPlus className={styles.cartBtn.icon}/>
+          </button>
+
+          <h1 className={styles.data.title}>
+            {Game.name}
+          </h1>
+
+          <h4 className={styles.data.weight}>
+            Peso: {GetWeight()}
+          </h4>
+
+          <ul className={styles.data.genderList}>
+            <h2 className={styles.data.genderTitle}>
+              Género/s:
+            </h2>
+            {Game.gender.map((data,i)=>(
+              <li
+                className={styles.data.genderTxt}
+                key={i}
+              >
+                {data}
+              </li>
+            ))}
+          </ul>
+
+          <h2 className={styles.data.price}>
+            s/{Game.price}
+          </h2>
+        </div>
+
+      </div>
+    </article>
   )
 }
--- src/Layout/Home.tsx
+++ src/Layout/Home.tsx
@@ -1,10 +1,11 @@
 import { Header } from "../Components/Header/Header"
-
+import { ProductCard } from "../Components/ProductCard/ProductCard"
+import { db } from "../DataBase/Test"
 
 export const Home = () => {
     const styles = {
         main: 'w-full min-h-screen flex relative',
-        content: 'size-full flex flex-col',
+        content: 'size-full flex flex-col dark:bg-slate-900',
 
     }
 
@@ -12,6 +13,15 @@ export const Home = () => {
     <section className={styles.main}>
         <div className={styles.content}>
             <Header/>
+            <ul className='w-full min-h-96 border border-black pt-24 pb-16 flex flex-col px-8 gap-y-5'>
+              {db.map((data,i)=>(
+                <li
+                  key={i}
+                >
+                  <ProductCard Game={data}/>
+                </li>
+              ))}
+            </ul>
 
         </div>
     </section>
